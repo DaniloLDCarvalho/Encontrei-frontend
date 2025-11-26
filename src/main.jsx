b8876aa77f0cf6b1b5c6198edb8b19492bc782bf
ytuik <ytuik99@gmail.com>
2025-11-26 02:30:19 -0300

    Adicionando novas dependencias e o QueryClientProvider para lidar com as chamadas de Api

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import App from './App'
 import './styles.css'
+import {queryClient} from "@/lib/queryClient";
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+          <ReactQueryDevtools initialIsOpen={false} /> {/* Ajuda a debugar as requests */}
+        </BrowserRouter>
+      </QueryClientProvider>
   </React.StrictMode>
 )
